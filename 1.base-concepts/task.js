47c7d932bf8aa6918ac2d3278961f13a47733e77
Nadezhda Kharkova <n13kharkova@gmail.com>
2022-11-30 17:14:52 +0300

    first exercise

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
+  const arr = [];
+  const d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) {
+    return arr;
+  }
+
+  if (d === 0) {
+    arr.push (-b / (2 * a));
+  }
+
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  }
   
   return arr;
-}
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
 }
