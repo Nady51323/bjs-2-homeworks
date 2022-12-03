2066d95ce7bead0b3a3ec79500ffc8decbe4ebfd
Nadezhda Kharkova <n13kharkova@gmail.com>
2022-12-03 11:40:38 +0300

    First task completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,12 +4,8 @@ function solveEquation(a, b, c) {
   const arr = [];
   const d = Math.pow(b, 2) - 4 * a * c;
 
-  if (d < 0) {
-    return arr;
-  }
-
   if (d === 0) {
-    arr.push (-b / (2 * a));
+    arr.push(-b / (2 * a));
   }
 
   if (d > 0) {
@@ -17,6 +13,25 @@ function solveEquation(a, b, c) {
     let x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x1, x2);
   }
+
+  // if (d < 0) {
+  //}
   
   return arr;
+}
+
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  }
+
+  const monthRate = percent / 12 / 100; // процентная ставка в месяц
+  const loanBody = amount - contribution; // тело кредита
+  const payment = loanBody * (monthRate + (monthRate / (((1 + monthRate) ** countMonths) - 1))); // вычисляем ежемесячный платеж
+  const totalSum = Number((payment * countMonths).toFixed(2));// общая сумма, которую придется заплатить клиенту
+    
+    return totalSum;   
+  
 }
