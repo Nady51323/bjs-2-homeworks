398c65db3f4118d51d3fc85031057cd582840673
Nadezhda Kharkova <n13kharkova@gmail.com>
2022-12-07 10:45:01 +0300

    Third task completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
+
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length){
+        return arr1.every((item, index) => item === arr2[index]); 
+    } else {
+        return false;
+    }
+   
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+
+    return users.filter(item => item.gender === gender).map(item => item.age).reduce((acc, item, index, arr) => {
+        acc += item;
+        if(index === arr.length - 1){    
+            return acc / arr.length;
+        }
+        return acc;    
+    }, 0)
   
 }
